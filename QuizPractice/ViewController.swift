750f310fa3e8bf76d8b93c4c2986a2c953feeee2
HUWENBO <>
2022-05-25 23:52:25 +0800

    create project

---
+++ QuizPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizPractice
+//
+//  Created by HWB on 2022/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
