06aaf757c117cfe9238f08cb17b4609b31fbe2a9
HUWENBO <>
2022-05-26 01:36:19 +0800

    read quiz.csv

--- QuizPractice/ViewController/QuizViewController.swift
+++ QuizPractice/ViewController/QuizViewController.swift
@@ -9,18 +9,32 @@ import UIKit
 
 class QuizViewController: UIViewController {
 
+    var csvArray: [String] = []
     
-    
-    
+    @IBOutlet weak var quizText: UITextView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        csvArray = loadCSV(fileName: "quiz")
 
         // Do any additional setup after loading the view.
     }
     
     @IBAction func btnAction(_ sender: UIButton) {
         print(sender.tag)
+    }
+    
+    func loadCSV(fileName: String) -> [String] {
+        let csvBundle = Bundle.main.path(forResource: fileName, ofType: "csv")!
+        do{
+            let csvData = try String(contentsOfFile: csvBundle,encoding: String.Encoding.utf8)
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            csvArray = lineChange.components(separatedBy: "\n")
+            csvArray.removeLast()
+        } catch {
+            print("エラー")
+        }
+        return csvArray
     }
 }
