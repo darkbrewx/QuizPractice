976daeb506be04d01d3e543b70f4478aead31af1
HUWENBO <>
2022-05-26 01:13:46 +0800

    add button tag

--- QuizPractice/ViewController/QuizViewController.swift
+++ QuizPractice/ViewController/QuizViewController.swift
@@ -20,15 +20,7 @@ class QuizViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnAction(_ sender: UIButton) {
+        print(sender.tag)
     }
-    */
-
 }
