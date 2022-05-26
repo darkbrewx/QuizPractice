6089872999a620cba3887eaf597caf5332bdf98f
HUWENBO <>
2022-05-26 10:47:26 +0800

    show quiz in storyboard

--- QuizPractice/ViewController/QuizViewController.swift
+++ QuizPractice/ViewController/QuizViewController.swift
@@ -9,14 +9,29 @@ import UIKit
 
 class QuizViewController: UIViewController {
 
-    var csvArray: [String] = []
+    @IBOutlet weak var quizNumberLabel: UILabel!
+    @IBOutlet weak var quizTextView: UITextView!
+    @IBOutlet weak var answerButton1: UIButton!
+    @IBOutlet weak var answerButton2: UIButton!
+    @IBOutlet weak var answerButton3: UIButton!
+    @IBOutlet weak var answerButton4: UIButton!
     
-    @IBOutlet weak var quizText: UITextView!
+    var csvArray: [String] = []
+    var quizArray: [String] = []
+    var quizCount = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         csvArray = loadCSV(fileName: "quiz")
+        print(csvArray)
+        quizArray = csvArray[quizCount].components(separatedBy: ",")
+        quizNumberLabel.text = "第\(quizCount + 1)問"
+        quizTextView.text = quizArray[0]
+        answerButton1.setTitle(quizArray[2], for: .normal)
+        answerButton2.setTitle(quizArray[3], for: .normal)
+        answerButton3.setTitle(quizArray[4], for: .normal)
+        answerButton4.setTitle(quizArray[5], for: .normal)
 
         // Do any additional setup after loading the view.
     }
